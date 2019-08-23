dbc77abae5dd52b4d326540dada1a6d738acacd3
Kilyakus <aleksandr.kilyakov@gmail.com>
2019-08-24 02:17:04 +0500

    initial commit

---
+++ RangeAsset.php
@@ -0,0 +1,22 @@
+<?php
+namespace kilyakus\range;
+
+class RangeAsset extends \yii\web\AssetBundle
+{
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . '/assets';
+    }
+    public $css = [
+        'css/bootstrap-range.css',
+    ];
+    public $js = [
+        'js/bootstrap-range.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+    public $jsOptions = [
+        'position' => \yii\web\View::POS_HEAD
+    ];
+}
